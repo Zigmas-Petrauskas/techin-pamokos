c76a72367a2f6056de2edcd520f59f5a06d48f73
Zigmas-Petrauskas <zigmasdev@gmail.com>
2026-09-15 15:20:39 +0300

    asdsd

--- backend/praktiniai/Užduotis_1/restaurant_management_api/app/app.js
+++ backend/praktiniai/Užduotis_1/restaurant_management_api/app/app.js
@@ -1,6 +1,6 @@
 const { restaurantName, city, isOpen } = require("./restaurant.js");
-console.log("Restaurant Management API");
+console.log("Restaurant Management API");
 console.log();
-console.log(`Restaurant: ${restaurantName}`);
-console.log(`City: ${city}`);
-console.log(`Open: ${isOpen}`);
+console.log(`Restaurant: ${restaurantName}`);
+console.log(`City: ${city}`);
+console.log(`Open: ${isOpen}`);
