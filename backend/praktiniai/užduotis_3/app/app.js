d248b96878b93c0dddc699bd8b9431d4195e23d2
Zigmas-Petrauskas <zigmasdev@gmail.com>
2026-09-16 18:44:35 +0300

    sdfdfsddddddddddddddddddfgdfg

---
+++ backend/praktiniai/užduotis_3/app/app.js
@@ -0,0 +1,6 @@
+import { restaurantName, city, isOpen } from './restaurant.js';
+console.log('Restaurant Management API');
+console.log();
+console.log(`Restaurant: ${restaurantName}`);
+console.log(`City: ${city}`);
+console.log(`Open: ${isOpen}`);
