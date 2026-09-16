42b5ab01321e9389c920fa0ecd280db7ffddaf0c
Zigmas-Petrauskas <zigmasdev@gmail.com>
2026-09-16 19:19:09 +0300

    tyyuuuuuuuuuuuuuuuuuutyutyuty

---
+++ backend/praktiniai/restaurant_management_api/app/restaurant.js
@@ -0,0 +1,4 @@
+const restaurantName = "Bella Napoli";
+const city = "Vilnius";
+const isOpen = true;
+export { restaurantName, city, isOpen };
---
+++ backend/praktiniai/restaurant_management_api/eslint.config.js
@@ -0,0 +1,37 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+
+    rules: {
+      "no-unused-vars": "warn",
+      eqeqeq: "error",
+      curly: "error",
+    },
+  },
+
+  eslintPluginPrettierRecommended,
+
+  {
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+          tabWidth: 2,
+          useTabs: false,
+          trailingComma: "all",
+          printWidth: 80,
+        },
+      ],
+    },
+  },
+]);
